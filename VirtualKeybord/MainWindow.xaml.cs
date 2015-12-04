0f105c01acb49121c5d5c4d3e13e75f797a6433b
Jason Magee <contact@jasemagee.com>
2015-12-04 22:11:50 +0000

    * Generated keys in a WPF wrap layout * Added code to make the WPF window not become active

---
+++ VirtualKeybord/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+
+namespace VirtualKeybord
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int ButtonSize = 60;
+
+        private const int WsExNoactivate = 0x08000000;
+        private const int GwlExstyle = -20;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (var c = 'A'; c <= 'Z'; c++)
+            {
+                wrapPanel.Children.Add(new Button {Content = c, Width = ButtonSize, Height = ButtonSize});
+            }
+
+            for (var i = 0; i < 9; i++)
+            {
+                wrapPanel.Children.Add(new Button {Content = i.ToString(), Width = ButtonSize, Height = ButtonSize});
+            }
+
+            foreach (Button button in wrapPanel.Children)
+            {
+                button.Click += Button_Click;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null)
+            {
+                return;
+            }
+           
+
+            // TODO: Send key
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            var interopHelper = new WindowInteropHelper(this);
+            var exStyle = GetWindowLong(interopHelper.Handle, GwlExstyle);
+            SetWindowLong(interopHelper.Handle, GwlExstyle, exStyle | WsExNoactivate);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+    }
+}
